952e5ecba1c76a18ec7f09a8d0e625702d6c4c73
justshiv <accounts@justshiv.com>
2015-10-08 17:59:22 +0200

    Correctly marking MRTs and SUS

--- analysis.js
+++ analysis.js
@@ -4,15 +4,40 @@
 
     MRT_ANSWERS = [[1, 3], [1, 4], [2, 4], [2, 3], [1, 3], [1, 4], [2, 4], [2, 3], [2, 4], [1, 4], [3, 4], [2, 3], [1, 2], [2, 4], [2, 3], [1, 4], [2, 4], [2, 3], [1, 3], [1, 4], [2, 4], [2, 3], [1, 4], [1, 3]];
 
+    //scoring MRT given the model answers
     var scoreMRT = function(answers){
-        mrt = 0;
+        var score = 0;
 
         for(var i = 0; i < answers.length; i++){
-            if(answers[i][0] === MRT_ANSWERS[i][0]){
-                mrt++;
+            if(answers[i][0] === MRT_ANSWERS[i][0] && answers[i][1] === MRT_ANSWERS[i][1]){
+                score++;
             }
         }
 
-        return mrt;
+        return score;
+    };
+
+    //scoring the SUS given the formula found in
+    //SUS: A Quick and Dirty Usability Scale by John Brooke
+    //http://www.usabilitynet.org/trump/documents/Suschapt.doc
+    var scoreSUS = function(answers){
+        var score = 0;
+
+        for(var i = 0; i < answers.length; i++){
+            var current = answers[i];
+
+            if(i+1 % 2 == 0){
+                current--;
+            }
+            else{
+                current = 5 - current;
+            }
+            score += current;
+        }
+
+        score *= 2.5;
+
+        return score;
+
     };
 
